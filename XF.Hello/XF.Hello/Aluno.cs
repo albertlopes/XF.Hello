56615bb00410667860da1bae86f45c02513d605d
albertlopes <albert.abt@gmail.com>
2018-10-23 22:54:04 -0300

    commit

---
+++ XF.Hello/XF.Hello/Aluno.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XF.Hello
+{
+    class Aluno
+    {
+        public Guid Id { get; set; }
+
+        public string Nome
+        {   get ; set;
+        }
+
+        public string Email
+        {
+            get ; set;
+        }
+
+
+    }
+}
--- XF.Hello/XF.Hello/DataBindingPage.xaml.cs
+++ XF.Hello/XF.Hello/DataBindingPage.xaml.cs
@@ -9,12 +9,26 @@ using Xamarin.Forms.Xaml;
 
 namespace XF.Hello
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class DataBindingPage : ContentPage
-	{
-		public DataBindingPage ()
-		{
-			InitializeComponent ();
-		}
-	}
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DataBindingPage : ContentPage
+    {
+        public DataBindingPage()
+        {
+            InitializeComponent();
+
+            Aluno aluno = inicializarAluno();
+            BindingContext = aluno;
+        }
+
+        private Aluno inicializarAluno()
+        {
+            Aluno aluno = new Aluno()
+            {
+                Id = Guid.NewGuid(),
+                Nome = "Albert",
+                Email = "albert.lopes@gmail.com"
+            };
+            return aluno;
+        }
+    }
 }
